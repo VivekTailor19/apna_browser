038657fab7a63d5abd1079b103f65f888429c1dc
Vivek <tailorvivekgec19@gmail.com>
2023-05-17 11:20:19 +0530

    web browser using flutter

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,30 @@
 import 'package:advanceflutter/provider/apnaprovider.dart';
 import 'package:advanceflutter/screen/apnasearch.dart';
+import 'package:device_preview/device_preview.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:sizer/sizer.dart';
 
 void main() {
   runApp(
-    MultiProvider(
-      providers: [
-        ChangeNotifierProvider(create: (context) => ApnaProvider(),)
-      ],
-      child: Sizer(
-        builder: (context, orientation, deviceType) => MaterialApp(
-          debugShowCheckedModeBanner: false,
-          routes: {
-            "/":(context) => ApnaSearch(),
+    // DevicePreview(
+    //    enabled: !kReleaseMode,
+    //    builder: (context) =>
+      MultiProvider(
+         providers: [
+          ChangeNotifierProvider(create: (context) => ApnaProvider(),)
+        ],
+       child: Sizer(
+          builder: (context, orientation, deviceType) => MaterialApp(
+           debugShowCheckedModeBanner: false,
+            routes: {
+              "/":(context) => ApnaSearch(),
 
           },
         ),
       ),
     ),
+      // ),
   );
 }
