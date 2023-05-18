06a323202987d3e0df9b3cc41d396653f48b4655
Vivek <tailorvivekgec19@gmail.com>
2023-05-18 09:48:12 +0530

    web browser app

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:advanceflutter/provider/apnaprovider.dart';
 import 'package:advanceflutter/screen/apnasearch.dart';
+import 'package:advanceflutter/screen/opera/operafirst.dart';
 import 'package:device_preview/device_preview.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
@@ -8,9 +9,9 @@ import 'package:sizer/sizer.dart';
 
 void main() {
   runApp(
-    // DevicePreview(
-    //    enabled: !kReleaseMode,
-    //    builder: (context) =>
+    DevicePreview(
+       enabled: !kReleaseMode,
+       builder: (context) =>
       MultiProvider(
          providers: [
           ChangeNotifierProvider(create: (context) => ApnaProvider(),)
@@ -18,13 +19,15 @@ void main() {
        child: Sizer(
           builder: (context, orientation, deviceType) => MaterialApp(
            debugShowCheckedModeBanner: false,
+            initialRoute: "opera",
             routes: {
               "/":(context) => ApnaSearch(),
+              "opera":(context) => Opera_First(),
 
           },
         ),
       ),
     ),
-      // ),
+      ),
   );
 }
---
+++ lib/screen/opera/operafirst.dart
@@ -0,0 +1,61 @@
+
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class Opera_First extends StatefulWidget {
+  const Opera_First({Key? key}) : super(key: key);
+
+  @override
+  State<Opera_First> createState() => _Opera_FirstState();
+}
+
+class _Opera_FirstState extends State<Opera_First> {
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+
+        body: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 15),
+          child: Column(
+            children: [
+              Row(mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                 IconButton(icon: Icon(Icons.loop), onPressed: () {}),
+                ],
+              ),
+
+              TextField(
+
+                style: TextStyle(fontSize: 15,color: Colors.black54),
+                decoration: InputDecoration(
+                    prefixIcon: Container(margin:EdgeInsets.only(left: 15,right: 8),
+                        child: Image.network("https://cdn1.iconfinder.com/data/icons/google-s-logo/150/Google_Icons-09-512.png",
+                          height: 35,width: 35,)),
+                    suffixIcon: Padding(
+                      padding: EdgeInsets.symmetric(horizontal: 15),
+                      child: Row(mainAxisAlignment: MainAxisAlignment.end,
+                        mainAxisSize: MainAxisSize.min,
+                        children: [
+                          Icon(Icons.mic_rounded,size: 25,color: Colors.black45,),
+                          IconButton(icon: Icon(Icons.search_rounded),iconSize: 25,color: Colors.blue,onPressed: (){},),
+                        ],
+                      ),
+                    ),
+                  hintText: "Search here...",
+                  hintStyle: TextStyle(color: Colors.black26),
+
+
+                  border: OutlineInputBorder(borderRadius: BorderRadius.circular(25),)
+
+                ),
+              ),
+
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
